697cc8ae6b42000d9423333eb68f50256d4e145c
paragithubit <paragtanna790@gmail.com>
2024-02-01 18:50:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+import TestForm from './components/TestForm';
+//import About from './components/About';
+import {useState} from 'react'
 
 function App() {
+      /* Enable dark mode in TestForm */
+    const [mode,setMode]=useState ('light');
+    const toogleMode =()=>{
+        if(mode=== 'dark'){
+           setMode('light')
+           document.body.style.backgroundColor='white';
+             showAlert('Enable Light mode ','success');
+        }
+          else{
+             setMode('dark')
+             document.body.style.backgroundColor='#4670af';
+             showAlert('Enable Dark mode ','success');
+          }
+    }
+          /* Alert code */
+          const [alert,setAlert]=useState('')
+          const showAlert =(message,type)=>{
+              setAlert({
+                   msg:message,
+                   type:type
+              })
+          } 
+
+          setTimeout(() => {
+                setAlert('');
+          }, 1500);
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <Navbar title = 'Home' about= 'About' mode={mode} toogleMode={toogleMode}/>
+       <Alert alert = {alert}/>
+       <div className="container">
+           <TestForm mode ={mode}  showAlert={ showAlert} heading='Enter the Text'/>
+            {/* <About/>*/}
+         </div>
+    </> 
+
+     );
 }
 
 export default App;
